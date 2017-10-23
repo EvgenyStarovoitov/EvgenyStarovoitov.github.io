2eb8544da3b333d26155c3cd06d10e5d3a36aa8c
mori4ok <qit91@mail.ru>
2017-10-23 11:09:00 +0300

    add all need func

---
+++ calendar/js/calendar.js
@@ -0,0 +1,85 @@
+var calendar = {
+  month: document.querySelectorAll('.init-month')[0],
+  next: document.querySelectorAll('.btn-next')[0],
+  prev: document.querySelectorAll('.btn-prev')[0],
+  label: document.querySelectorAll('.label-month')[0],
+  activeDates: null,
+  date: new Date(),
+  todaysDate: new Date(),
+  monthArr : ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+
+  // Initialize pointer on first days, create table month, create all listeners(button)
+  init: function () {
+    this.date.setDate(1)
+    this.createMonth()
+    this.createListeners()
+  },
+
+  createListeners: function () {
+    var _this = this
+    //  Clear a calendar and show the next month
+    this.next.addEventListener('click', function () {
+      _this.clearCalendar()
+      var nextMonth = _this.date.getMonth() + 1
+      _this.date.setMonth(nextMonth)
+      _this.createMonth()
+    })
+    // Clear a calendar and show the prev month
+    this.prev.addEventListener('click', function () {
+      _this.clearCalendar()
+      var prevMonth = _this.date.getMonth() - 1
+      _this.date.setMonth(prevMonth)
+      _this.createMonth()
+    })
+  },
+  // Create the days in month table
+  createDay: function (num, day, year) {
+    var newDay = document.createElement('div')
+    var dateEl = document.createElement('span')
+    dateEl.className = 'date__num';
+    newDay.className = 'cal__date';
+    dateEl.innerHTML = num;
+
+    if (num === 1) {
+      var offset = ((day ) * 14.28)
+      if (offset > 0) {
+        newDay.style.marginLeft = offset + '%'
+      }
+    };
+
+    if (this.date.getTime() <= this.todaysDate.getTime() - 1) {
+      newDay.classList.add('cal__date--disabled')
+    } else {
+      newDay.classList.add('cal__date--active')
+      newDay.setAttribute('data-calendar-status', 'active')
+    }
+
+    if (this.date.toString() === this.todaysDate.toString()) {
+      newDay.classList.add('cal__date_today')
+    }
+
+    newDay.appendChild(dateEl)
+    this.month.appendChild(newDay)
+  },
+  // Create month table
+  createMonth: function () {
+    var currentMonth = this.date.getMonth()
+    while (this.date.getMonth() === currentMonth) {
+      this.createDay(this.date.getDate(), this.date.getDay(), this.date.getFullYear())
+      this.date.setDate(this.date.getDate() + 1)
+    }
+
+    this.date.setDate(1)
+    this.date.setMonth(this.date.getMonth() - 1)
+
+    this.label.innerHTML = this.monthsAsString(this.date.getMonth()) + ' ' + this.date.getFullYear()
+  },
+  //Return month from array
+  monthsAsString: function (monthIndex) {
+    return this.monthArr[monthIndex]
+  },
+  //Clear month and delete all days
+  clearCalendar: function () {
+    calendar.month.innerHTML = ''
+  },
+}
